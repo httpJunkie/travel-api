9554d45758b9012f01670635077605c277357ecc
Eric Bishard <e.bishard@gmail.com>
2021-10-04 03:22:06 -0700

    Remove id from Airline Schema

--- src/airlines/airlines.model.ts
+++ src/airlines/airlines.model.ts
@@ -1,7 +1,6 @@
 import { model, Schema } from 'ottoman'
 
 const AirlineSchema = new Schema({
-  id: { type: Boolean, required: true },
   name: { type: String, required: true },
   callsign: { type: String, required: true }
 })
